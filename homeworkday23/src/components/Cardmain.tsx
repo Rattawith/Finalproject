e6051529f2abfeefc25902cfde777937c3759fe3
Rattawith <patipan_007@hotmail.com>
2024-08-27 17:00:06 +0700

    req create main
    create main

---
+++ homeworkday23/src/components/Cardmain.tsx
@@ -0,0 +1,71 @@
+import './Cardmain.css'; 
+
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  imageUrl: string;
+  price: number;
+}
+
+const products: Product[] = [
+  {
+    id: 1,
+    title: 'Product 1',
+    description: 'This is the first product.',
+    imageUrl: 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6409/6409982_sd.jpg;maxHeight=640;maxWidth=550',
+    price: 29.99,
+  },
+  {
+    id: 2,
+    title: 'Product 2',
+    description: 'This is the second product.',
+    imageUrl: 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6409/6409982_sd.jpg;maxHeight=640;maxWidth=550',
+    price: 39.99,
+  },
+  {
+    id: 3,
+    title: 'Product 3',
+    description: 'This is the third product.',
+    imageUrl: 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6409/6409982_sd.jpg;maxHeight=640;maxWidth=550',
+    price: 49.99,
+  },
+  {
+    id: 4,
+    title: 'Product 3',
+    description: 'This is the third product.',
+    imageUrl: 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6409/6409982_sd.jpg;maxHeight=640;maxWidth=550',
+    price: 49.99,
+  },
+  {
+    id: 5,
+    title: 'Product 3',
+    description: 'This is the third product.',
+    imageUrl: 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6409/6409982_sd.jpg;maxHeight=640;maxWidth=550',
+    price: 49.99,
+  },
+  {
+    id: 6,
+    title: 'Product 3',
+    description: 'This is the third product.',
+    imageUrl: 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6409/6409982_sd.jpg;maxHeight=640;maxWidth=550',
+    price: 49.99,
+  },
+];
+
+function Cardmain() {
+  return (
+    <div className="productList">
+      {products.map((product) => (
+        <div key={product.id} className="card">
+          <img src={product.imageUrl} alt={product.title} />
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          <p>${product.price.toFixed(2)}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Cardmain;
---
+++ homeworkday23/src/pages/Main.tsx
@@ -0,0 +1,14 @@
+import Cardmain from "../components/Cardmain"
+import Navmain from "../components/Navmain"
+import './Main.css';
+
+function main() {
+  return (
+    <div>
+        <Navmain></Navmain>
+        <Cardmain></Cardmain>
+    </div>
+  )
+}
+
+export default main
