c40619003cde4071e56e734ee3759e138de0a8e4
Rattawith <patipan_007@hotmail.com>
2024-09-19 16:35:43 +0700

    Req. ตกแต่งหน้า Product
    ทำให้หน้า Product สมบูรณ์

--- homeworkday23/src/components/Header.tsx
+++ homeworkday23/src/components/Header.tsx
@@ -1,12 +1,18 @@
-
+import './Header.css';
 
 function Header() {
   return (
-    <div>
-        <img src="https://img.icons8.com/?size=80&id=ledoOw5M8qvM&format=png" alt="logobrand" />
+    <div className='header'>
+    <div className='header__bar'>
+        <a href="#"><img src="https://img.icons8.com/?size=80&id=ledoOw5M8qvM&format=png" alt="logobrand" /></a>
         <a href="#">Home</a>
         <a href="#">Products</a>
         <a href="#">Contact</a>
+    </div>
+    <div className="header__icon">
+            <a href="#"><img src="https://img.icons8.com/?size=50&id=36389&format=png" alt="menu" />
+            </a>
+        </div>
     </div>
   )
 }
